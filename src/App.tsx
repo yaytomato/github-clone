0acf45fca42737c6fa4621e8b2cee123d9e7a6f2
yaytomato <yehoon_s@naver.com>
2021-02-16 00:06:43 +0900

    Add react-router for routing + Route /new to page component New

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-function App() {
+import New from "./pages/New";
+
+interface Props {}
+
+export const App: React.FC<Props> = ({}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/new" exact component={New} />
+      </Switch>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/pages/New.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {}
+
+export const New: React.FC<Props> = ({}) => {
+  return <div>new</div>;
+};
+
+export default New;
